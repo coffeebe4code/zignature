4ea93126945c0f6e93bfa8cdf1938a7efec5a62b
Chris <chris.clark@coffeeb4code.com>
2023-04-16 20:04:58 -0500

    upgrading to 11

--- build.zig
+++ build.zig
@@ -4,35 +4,35 @@ const std = @import("std");
 const c_opts = [_][]const u8{ "-std=c11", "-Wall", "-Werror", "-Wextra" };
 
 const libs = struct {
-    const tcp_server = std.build.Pkg{
+    const tcp_server = .{
         .name = "tcp_server",
         .source = .{ .path = "lib/tcp_server.zig" },
-        .dependencies = &[_]std.build.Pkg{tcp_config},
+        .dependencies = &.{tcp_config},
     };
-    const tcp_config = std.build.Pkg{
+    const tcp_config = .{
         .name = "tcp_config",
         .source = .{ .path = "lib/tcp_config.zig" },
-        .dependencies = &[_]std.build.Pkg{},
+        .dependencies = &.{},
     };
-    const http_config = std.build.Pkg{
+    const http_config = .{
         .name = "http_config",
         .source = .{ .path = "lib/http_config.zig" },
-        .dependencies = &[_]std.build.Pkg{},
+        .dependencies = &.{},
     };
-    const config = std.build.Pkg{
+    const config = .{
         .name = "config",
         .source = .{ .path = "lib/config.zig" },
-        .dependencies = &[_]std.build.Pkg{},
+        .dependencies = &.{},
     };
-    const http = std.build.Pkg{
+    const http = .{
         .name = "http",
         .source = .{ .path = "lib/http.zig" },
-        .dependencies = &[_]std.build.Pkg{},
+        .dependencies = &.{},
     };
-    const http_parser = std.build.Pkg{
+    const http_parser = .{
         .name = "http_parser",
         .source = .{ .path = "lib/http_parser.zig" },
-        .dependencies = &[_]std.build.Pkg{ http, config, http_config },
+        .dependencies = &.{ http, config, http_config },
     };
 };
 
@@ -41,28 +41,38 @@ pub fn build(b: *std.build.Builder) void {
 }
 
 fn buildPlaintext(b: *std.build.Builder) void {
-    const m = b.standardReleaseOptions();
-    const exe = b.addExecutable("plaintext", "src/plaintext.zig");
+    const m = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
+    const exe = b.addExecutable(.{
+        .name = "plaintext",
+        .root_source_file = .{ .path = "src/plaintext.zig" },
+        .target = target,
+        .optimize = m,
+    });
 
-    const tests = b.addTest(libs.http_parser.source.path);
-    tests.addPackage(libs.tcp_config);
-    tests.addPackage(libs.http);
-    tests.addPackage(libs.http_config);
-    tests.setTarget(target);
-    tests.setBuildMode(m);
+    const tests = b.addTest(.{
+        .name = "tests",
+        .root_source_file = libs.http_parser.source,
+        .target = target,
+        .optimize = m,
+    });
+    tests.addModule(libs.tcp_config.name, @as(*std.Build.Module, &libs.tcp_config));
+    tests.addModule(libs.http.name, libs.http);
+    tests.addModule(libs.http_config.name, libs.http_config);
+    //tests.setTarget(target);
+    //tests.setBuildMode(m);
 
     const tests_step = b.step("test", "testing");
     tests_step.dependOn(&tests.step);
 
-    exe.setTarget(target);
-    exe.setBuildMode(m);
+    //exe.setTarget(target);
+    //exe.setBuildMode(m);
     exe.addLibraryPath("lib");
-    exe.addPackage(libs.tcp_config);
-    exe.addPackage(libs.http_config);
-    exe.addPackage(libs.tcp_server);
-    exe.addPackage(libs.http);
-    exe.addPackage(libs.http_parser);
+    exe.addModule(libs.tcp_config);
+    exe.addModule(libs.http_config);
+    exe.addModule(libs.tcp_server);
+    exe.addModule(libs.http);
+    exe.addModule(libs.http_parser);
 
     exe.addIncludePath("c");
     exe.linkLibC();
